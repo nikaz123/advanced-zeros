e5ce525849eaf888807d5d5e7c82563e9b095395
Lenovo <nikaz@tut.by>
2018-03-05 09:12:20 +0300

    second

--- src/index.js
+++ src/index.js
@@ -1,68 +1,71 @@
 module.exports = function getZerosCount(number, base) {
-
     console.log(number);
 
-
     let countZero = 0;
-
     let j = 1;
     let i = 2;
+    let powers = [];
+    let results = [];
+
     let a = new Array();
-    do
-    {
-        if (base % i === 0)
-        {
+    do {
+        if (base % i == 0) {
+            if (typeof powers[i] === 'undefined' && (i != base)) powers[i] = 1
+            else if (i != base) powers[i] += 1;
             a[j] = i;
-            j++;
+            if (i != base) j++;
             base = base / i;
         }
-        else
-        {i++;  }
+        else {
+            i++;
+        }
     }
     while (i < base);
     a[j] = i;
+    if (typeof powers[i] === 'undefined') powers[i] = 1
+    else powers[i] += 1;
 
-    console.log(a[j]);
 
-    let step=0;
-
-    for (let i=1; i<a.length; i++) {
-        if (a[i]===a[j] ) step = step + a[j];
+    bbase = 0;
+    for (let i = 1; i < a.length; i++) {
+        if (a[i] === a[j]) {
+            bbase++
+        };
 
     }
-    console.log(step);
 
-    let k=a[j];
-let track=0;
-     for (let i = step; i <= number; i = i + step) {
+    for (dapower in powers) {
+        dapower = Number(dapower)
+        let a = Math.floor(getPower(dapower, number, 1) / powers[dapower])
+        results.push(a)
+    }
 
-        let j = 0;
-
-       let ii = i;
-
-        do {
-     if (ii % k === 0)
-          {
-
-              j++;
-                ii = ii / k;
-            }
-       //     console.log("wow" + ii);
-       }
-
-         while (ii % k === 0);
-       countZero = countZero + j;
-       //  console.log(countZero);
-track=i;
-   }
-
-    console.log(track);
+    countZero = Math.min.apply(null, results);
     return (countZero);
 
 
+    function getPower(pk, pnumber, debug=0) {
+        let cz = 0;
+        let i = 0;
 
+        for (i = pk; i <= pnumber; i = i + pk) {
+            let ii = i;
+            let j = 0;
 
+            do {
 
+                if (ii % pk == 0) {
 
-    // your implementation
+                    ii = ii / pk;
+                    j++;
+                }
+            }
+            while (ii % pk == 0);
+            cz = cz + j;
+             }
+
+        return cz;
+    }
+
 }
+
